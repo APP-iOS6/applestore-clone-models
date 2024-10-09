a824729ea5fa322957cffc591b37197271c08051
sueun kim <doms@kakao.com>
2024-10-09 23:01:24 +0900

    [del]: AuthManager 사용하지 않는 코드 삭제

--- Sources/applestore-clone-models/applestore_clone_models.swift
+++ Sources/applestore-clone-models/applestore_clone_models.swift
@@ -22,117 +22,15 @@ public enum AuthenticationFlow {
 
 @MainActor
 public class AuthManager: ObservableObject {
-    @Published var name: String = "unkown"
-    @Published var email: String = ""
-    @Published var password: String = ""
-    @Published var confirmPassword: String = ""
     @Published var flow: AuthenticationFlow = .login
-    @Published var isValid: Bool  = false
     @Published var authenticationState: AuthenticationState = .unauthenticated
     @Published var errorMessage: String = ""
-    @Published var user: User?
-    @Published var displayName: String = ""
-    @Published var photoURL: URL?
     
     @Published public var userID: String = ""
     @Published public var itemStore: ItemStore = ItemStore()
     
-    init() {
-//        registerAuthStateHandler()
-        
-        $flow
-            .combineLatest($email, $password, $confirmPassword)
-            .map { flow, email, password, confirmPassword in
-                flow == .login
-                ? !(email.isEmpty || password.isEmpty)
-                : !(email.isEmpty || password.isEmpty || confirmPassword.isEmpty)
-            }
-            .assign(to: &$isValid)
-    }
-    
-    private var authStateHandler: AuthStateDidChangeListenerHandle?
-    
-    func registerAuthStateHandler() {
-        if authStateHandler == nil {
-            authStateHandler = Auth.auth().addStateDidChangeListener { auth, user in
-                self.user = user
-                self.authenticationState = user == nil ? .unauthenticated : .authenticated
-                self.displayName = user?.displayName ?? ""
-                self.photoURL = user?.photoURL
-            }
-        }
-    }
-    
-    func switchFlow() {
-        flow = flow == .login ? .signUp : .login
-        errorMessage = ""
-    }
-    
-    private func wait() async {
-        do {
-            print("Wait")
-            try await Task.sleep(nanoseconds: 1_000_000_000)
-            print("Done")
-        }
-        catch { }
-    }
-    
-    func reset() {
-        flow = .login
-        email = ""
-        password = ""
-        confirmPassword = ""
-    }
-}
-
-extension AuthManager {
-    func signInWithEmailPassword() async -> Bool {
-        authenticationState = .authenticating
-        do {
-            try await Auth.auth().signIn(withEmail: self.email, password: self.password)
-            return true
-        }
-        catch  {
-            print(error)
-            errorMessage = error.localizedDescription
-            authenticationState = .unauthenticated
-            return false
-        }
-    }
-    
-    func signUpWithEmailPassword() async -> Bool {
-        authenticationState = .authenticating
-        do  {
-            try await Auth.auth().createUser(withEmail: email, password: password)
-            return true
-        }
-        catch {
-            print(error)
-            errorMessage = error.localizedDescription
-            authenticationState = .unauthenticated
-            return false
-        }
-    }
-    
-    func signOut() {
-        do {
-            try Auth.auth().signOut()
-        }
-        catch {
-            print(error)
-            errorMessage = error.localizedDescription
-        }
-    }
-    
-    func deleteAccount() async -> Bool {
-        do {
-            try await user?.delete()
-            return true
-        }
-        catch {
-            errorMessage = error.localizedDescription
-            return false
-        }
+    static let shared = AuthManager()
+    private init() {
     }
 }
 
