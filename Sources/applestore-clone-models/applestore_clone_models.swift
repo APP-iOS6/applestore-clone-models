a13dad6e3fa1d5e9a77b1a7f259d52c5a1dca227
sueun kim <doms@kakao.com>
2024-10-10 01:25:30 +0900

    [reflector]: Model 생성자 접근 제어 수정

--- Sources/applestore-clone-models/applestore_clone_models.swift
+++ Sources/applestore-clone-models/applestore_clone_models.swift
@@ -91,6 +91,19 @@ public struct Item: Codable, Sendable {
     var color: String       // 색상
     var isAvailable: Bool   // 상품상태(품절, 판매중 / Bool)
     
+    public static let dummyData = Item(itemId: "1", name: "Dummy Item", category: "Dummy Category", price: 1000, description: "Dummy Description", stockQuantity: 10, imageURL: "https://example.com/image.jpg", color: "Dummy Color", isAvailable: true)
+    public init(itemId: String, name: String, category: String, price: Int, description: String, stockQuantity: Int, imageURL: String, color: String, isAvailable: Bool) {
+        self.itemId = itemId
+        self.name = name
+        self.category = category
+        self.price = price
+        self.description = description
+        self.stockQuantity = stockQuantity
+        self.imageURL = imageURL
+        self.color = color
+        self.isAvailable = isAvailable
+    }
+    
     // price가격을 천 단위 구분
     var formattedPrice: String {
         let numberFormatter = NumberFormatter()
@@ -121,6 +134,22 @@ public struct Order: Codable {
     
     var bankName: String               // 은행명
     var accountNumber: String          // 계좌번호
+    public init(trackingNumber: String, orderDate: Date, nickname: String, shippingAddress: String, phoneNumber: String, productName: String, imageURL: String, color: String, itemId: String, hasAppleCarePlus: Bool, quantity: Int, unitPrice: Int, bankName: String, accountNumber: String) {
+        self.trackingNumber = trackingNumber
+        self.orderDate = orderDate
+        self.nickname = nickname
+        self.shippingAddress = shippingAddress
+        self.phoneNumber = phoneNumber
+        self.productName = productName
+        self.imageURL = imageURL
+        self.color = color
+        self.itemId = itemId
+        self.hasAppleCarePlus = hasAppleCarePlus
+        self.quantity = quantity
+        self.unitPrice = unitPrice
+        self.bankName = bankName
+        self.accountNumber = accountNumber
+    }
     
     // *수량 + 애플 케어가 true라면 기존 가격에서 10% 더함
     var totalPrice: Int {
@@ -141,14 +170,27 @@ public struct UserID: Identifiable {
     public var id: UUID = UUID() // 고객 아이디
     var order: [Order]
     var profileInfo: ProfileInfo
+    
+    public init(order: [Order], profileInfo: ProfileInfo) {
+        self.order = order
+        self.profileInfo = profileInfo
+    }
 }
 // 고객 관리
-public struct ProfileInfo: Codable {
+public struct ProfileInfo: Codable, Sendable {
     var nickname: String               // 닉네임
     var email: String                  // 이메일
     var registrationDate: Date         // 가입날짜
     var recentlyViewedProducts: [String] // 최근 본 제품(상품 ID 리스트)
     
+    static let dummyData = ProfileInfo(nickname: "김민수", email: "minsoo@gmail.com", registrationDate: Date(), recentlyViewedProducts: [])
+   
+    public init(nickname: String, email: String, registrationDate: Date, recentlyViewedProducts: [String]) {
+        self.nickname = nickname
+        self.email = email
+        self.registrationDate = registrationDate
+        self.recentlyViewedProducts = recentlyViewedProducts
+    }
     // 가입 날짜 Formatter 생성
     var formattedRegistration: String {
         let formatter = DateFormatter()
